34ad34056cf530563a2570ed171ce171de5a0c74
Jkishi6 <jonathankishi6@gmail.com>
2022-10-02 00:02:05 -0700

    add book function added

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,40 @@
 
-// Your Code Here
+let addButton = document.querySelector("#add-book");
+
+let newId = 0;
+
+async function assignId () {
+    let response = await fetch('http://localhost:3001/listBooks')
+    
+    let data = await response.json();
+    
+    data.forEach(book => {
+        if (book.id > newId) {
+            newId = book.id;
+        }
+    })
+    return newId + 1;
+
+}
+
+addButton.addEventListener("click", async () =>{
+    
+    let id = await assignId();
+
+    let dataToSend = {
+        id: id, 
+        title: document.querySelector("#add-title").value,
+        year: document.querySelector("#add-year").value,
+        description: document.querySelector("#add-description").value,
+        quantity: document.querySelector("#add-quantity").value,
+        imageURL: document.querySelector("#add-imageURL").value,
+         }
+    
+    fetch('http://localhost:3001/addBook', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataToSend)
+    })
+})
